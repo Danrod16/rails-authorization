996c91cc539efac4668297cd1616b99e029fb90e
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-08-23 10:26:19 +0800

    Feat: added pundit to restaurants controller

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,33 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    @record.user == @user
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    edit?
+  end
+end
+
